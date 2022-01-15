f509773a661628a458d9717d24a8a86f9ade6ee6
valentinbootz <97349551+valentinbootz@users.noreply.github.com>
2022-01-15 13:14:33 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.5
+
+//
+// This source file is part of the Apodini open source project
+// 
+// SPDX-FileCopyrightText: 2021 Paul Schmiedmayer and the project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
+//
+
+import PackageDescription
+
+
+let package = Package(
+    name: "ApodiniTemplate",
+    platforms: [
+        .macOS(.v11)
+    ],
+    products: [
+        .library(name: "ApodiniTemplate", targets: ["ApodiniTemplate"])
+    ],
+    targets: [
+        .target(name: "ApodiniTemplate"),
+        .testTarget(
+            name: "ApodiniTemplateTests",
+            dependencies: [
+                .target(name: "ApodiniTemplate")
+            ]
+        )
+    ]
+)
